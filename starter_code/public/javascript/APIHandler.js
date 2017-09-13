5efc6bbb9c18333167ac1cc0fec13fa09c1303eb
mpascualach <miguelpacheson@hotmail.es>
2017-09-13 12:50:00 +0200

    first commit

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,39 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    return $.getJSON(`${this.BASE_URL}` + `/characters`).done(function(response) {
+      const characters = []
+      $.each(response, function(index) {
+        characters.push(response[index]);
+      })
+    });
+  }
+
+getOneRegister() {
+  this.getFullList();
+  const id = $("#character-id").val();
+  return $.get(`${this.BASE_URL}` + `/characters/` + id)
+  .done(function(response) {
+    $.each(response, function(index) {
+      if (characters[index].id == id) {
+        console.log(characters[index]);
+      }
+    })
+  })
+}
+
+
+
+  createOneRegister() {}
+
+  updateOneRegister() {
 
   }
 
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
+  deleteOneRegister() {
 
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,25 @@
-const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
+
+
+const charactersAPI = new APIHandler("http://ih-api.herokuapp.com");
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
-  })
+    charactersAPI.getFullList();
+  });
 
   $('#fetch-one').on('click', (e) => {
-
-  })
+    charactersAPI.getOneRegister();
+  });
 
   $('#delete-one').on('click', (e) => {
-
-  })
+    charactersAPI.deleteOneRegister();
+  });
 
   $('#edit-character-form').on('submit', (e) => {
-
-  })
+    charactersAPI.updateOneRegister();
+  });
 
   $('#new-character-form').on('submit', (e) => {
-
-  })
-})
+    charactersAPI.createOneRegister();
+  });
+});
